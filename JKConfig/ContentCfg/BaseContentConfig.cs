4b1febd7560c339e8adeed5634b36f8fcc9a8f8b
JacobG55 <jacobgorg5@gmail.com>
2026-07-09 12:33:01 -0400

    helper methods if u want

--- JKConfig/ContentCfg/BaseContentConfig.cs
+++ JKConfig/ContentCfg/BaseContentConfig.cs
@@ -1,5 +1,7 @@
 ﻿using BepInEx.Configuration;
 using LethalLevelLoader;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace JKConfig.ContentCfg
 {
@@ -25,6 +27,42 @@ namespace JKConfig.ContentCfg
         public V GetValue<V>(string propertyName, V defaultValue, string description = "")
         {
             return _file.Bind(Content.name, propertyName, defaultValue, description).Value;
+        }
+
+        public string Stringify(IEnumerable<StringWithRarity> properties)
+        {
+            return string.Join(',', properties.Select(x => $"{x.Name}-{x.Rarity}"));
+        }
+
+        public IEnumerable<StringWithRarity> ParseSwr(string config)
+        {
+            string[] split = config.Split(',');
+            foreach (string s in split)
+            {
+                string[] v = s.Trim().Split('-');
+                if (v.Length > 1 && int.TryParse(v[1], out int rarity))
+                {
+                    yield return new StringWithRarity(v[0].Trim(), rarity);
+                }
+            }
+        }
+
+        public string Stringify(IEnumerable<Vector2WithRarity> properties)
+        {
+            return string.Join(',', properties.Select(x => $"{x.Min}-{x.Max}-{x.Rarity}"));
+        }
+
+        public IEnumerable<Vector2WithRarity> ParseV2wr(string config)
+        {
+            string[] split = config.Split(',');
+            foreach (string s in split)
+            {
+                string[] v = s.Trim().Split('-');
+                if (v.Length > 2 && float.TryParse(v[0], out float x) && float.TryParse(v[1], out float y) && int.TryParse(v[2], out int rarity))
+                {
+                    yield return new Vector2WithRarity(x, y, rarity);
+                }
+            }
         }
     }
 }
